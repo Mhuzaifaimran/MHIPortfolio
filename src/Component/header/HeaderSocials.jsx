70798a9495323c75eb3df4108c5eba15b8b3f977
Majid <majid.cybus@gmail.com>
2022-06-23 15:38:00 +0500

    Header Socials

--- src/Component/header/HeaderSocials.jsx
+++ src/Component/header/HeaderSocials.jsx
@@ -6,13 +6,18 @@ import {BsInstagram} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
-    <div className='header__socials'>
-<a href='https://linkedin.com/in/mhuzaifaimran' target="blank"><BsLinkedin/></a>
-<a href='https://github.com' target="blank"><BsGithub/></a>
-<a href='https://instagram.com' target="blank"><BsInstagram/></a>
-
+    <div className="header__socials">
+      <a href="https://linkedin.com/in/mhuzaifaimran" target="blank">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/MHuzaifaimran" target="blank">
+        <BsGithub />
+      </a>
+      <a href="https://instagram.com/m_huzaifa_imran0" target="blank">
+        <BsInstagram />
+      </a>
     </div>
-  )
+  );
 }
 
 export default HeaderSocials;
